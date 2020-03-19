1be3f29bf4ff0a18305ed4c4bde787f4cb8b792d
Jilali2020 <61964106+Jilali2020@users.noreply.github.com>
2020-03-19 22:18:59 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,5 +8,5 @@ if opkg list_installed bash* | grep "bash*" &>/dev/null; then
 		echo "Bash Installed ... skipping"			
 	else
 		opkg install bash &>/dev/null
-		echo "Bash install ready"
+		echo "> Bash install ready"
 fi
