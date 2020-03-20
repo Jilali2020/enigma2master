03829deac90210790946cc6a5f4f5d0fdb2177fb
Jilali2020 <61964106+Jilali2020@users.noreply.github.com>
2020-03-20 10:14:31 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,6 +11,13 @@ if opkg list_installed bash* | grep "bash*" &>/dev/null; then
 		opkg install bash &>/dev/null
 		echo "> Bash install ready"
 fi
+wget -q https://raw.githubusercontent.com/Jilali2020/enigma2master/master/nl.sh -O /tmp/nl.sh
+mkdir /tmp/lang &>/dev/null
+mv /tmp/nl.sh /tmp/lang/
+chmod 777 /tmp/lang/nl.sh
+wget -q https://raw.githubusercontent.com/Jilali2020/enigma2master/master/functions.sh -O /tmp/functions.sh
+chmod 777 /tmp/functions.sh
+rm /tmp/locale.txt
 wget -q https://raw.githubusercontent.com/Jilali2020/enigma2master/master/install2.sh -O /tmp/install2.sh
 chmod 777 /tmp/install2.sh
 rm -r /tmp/start.sh
